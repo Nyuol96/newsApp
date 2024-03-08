95cb123adb2591d05c8349d23090c628156b3e83
Nyuol96 <121433290+Nyuol96@users.noreply.github.com>
2024-03-08 14:52:48 +0200

    creataed Categor View

---
+++ lib/widgets/categoryView.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CategoryView extends StatelessWidget {
+  const CategoryView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
